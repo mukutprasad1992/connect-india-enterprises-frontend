487bd881eaa95898fe8c3a4cb2390fe2d2fbf8f1
mukutprasad1992 <mukutprasad1992@gmail.com>
2025-05-11 15:01:53 +0530

    changes

--- src/app/(DashboardLayout)/utilities/investment/page.tsx
+++ src/app/(DashboardLayout)/utilities/investment/page.tsx
@@ -38,7 +38,6 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { DataGrid, GridToolbarColumnsButton, GridToolbarContainer } from "@mui/x-data-grid";
 import DashboardCard from "../../components/shared/DashboardCard";
 import { jwtDecode } from "jwt-decode";
-//import InvestmentDialog from '../AI/AiAssistantInvestment/page';
 import React from "react";
 
 const durations = [
