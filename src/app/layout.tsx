c81884ad025d0dc6c2355aa924040fed9e913b5a
Mukut Prasad <mukutprasad1996@gmail.com>
2025-05-15 18:25:43 +0530

    resolve Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,14 @@
-"use client";
-import { baselightTheme } from "@/utils/theme/DefaultColors";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { Box } from "@mui/material";
-import { usePathname } from "next/navigation";
+// app/layout.tsx
+import { ReactNode } from "react";
+import ClientWrapper from "../app/(DashboardLayout)/components/ClientWrapper";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const pathname = usePathname();
-  const isAuthPage = pathname === "/authentication/register" || pathname === "/authentication/login" || pathname === "/authentication/forgot_password" || pathname === "/authentication/reset_password";
-
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head />
       <body>
-        {isAuthPage ? (
-          <Box
-            sx={{
-              backgroundImage: 'url(https://picsum.photos/1000/700)', // Replace with your image path
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-              backdropFilter: 'blur(5px)', // Optional: Adds a blur effect for better readability
-            }}
-          >
-            <ThemeProvider theme={baselightTheme}>
-              <CssBaseline />
-              {children}
-            </ThemeProvider>
-          </Box>
-        ) : (
-          <ThemeProvider theme={baselightTheme}>
-            <CssBaseline />
-            {children}
-          </ThemeProvider>
-        )}
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   );
 }
-
